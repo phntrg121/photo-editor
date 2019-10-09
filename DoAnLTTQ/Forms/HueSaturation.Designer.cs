c707e4fad1694618d9b5cadee0109f4221406c3b
Hal5s1210 <51257908+Hal5s1210@users.noreply.github.com>
2019-10-09 23:16:34 +0700

    test

--- DoAnLTTQ/Forms/HueSaturation.Designer.cs
+++ DoAnLTTQ/Forms/HueSaturation.Designer.cs
@@ -17,6 +17,8 @@
             {
                 components.Dispose();
             }
+            image.Dispose();
+            adjusted.Dispose();
             base.Dispose(disposing);
         }
 
@@ -37,9 +39,13 @@
             this.trackBar2 = new System.Windows.Forms.TrackBar();
             this.trackBar1 = new System.Windows.Forms.TrackBar();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
             this.SuspendLayout();
             // 
             // label4
@@ -63,7 +69,7 @@
             // button2
             // 
             this.button2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.button2.Location = new System.Drawing.Point(237, 309);
+            this.button2.Location = new System.Drawing.Point(238, 353);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 10;
@@ -73,7 +79,7 @@
             // button1
             // 
             this.button1.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.button1.Location = new System.Drawing.Point(156, 309);
+            this.button1.Location = new System.Drawing.Point(157, 353);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 11;
@@ -83,20 +89,20 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(17, 277);
+            this.label2.Location = new System.Drawing.Point(14, 277);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.Size = new System.Drawing.Size(55, 13);
             this.label2.TabIndex = 8;
-            this.label2.Text = "Contrast";
+            this.label2.Text = "Saturation";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 239);
+            this.label1.Location = new System.Drawing.Point(42, 239);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.Size = new System.Drawing.Size(27, 13);
             this.label1.TabIndex = 9;
-            this.label1.Text = "Brightness";
+            this.label1.Text = "Hue";
             // 
             // trackBar2
             // 
@@ -107,14 +113,15 @@
             this.trackBar2.Name = "trackBar2";
             this.trackBar2.Size = new System.Drawing.Size(200, 45);
             this.trackBar2.TabIndex = 6;
+            this.trackBar2.Scroll += new System.EventHandler(this.TrackBar2_Scroll);
             // 
             // trackBar1
             // 
             this.trackBar1.AutoSize = false;
             this.trackBar1.LargeChange = 1;
             this.trackBar1.Location = new System.Drawing.Point(75, 230);
-            this.trackBar1.Maximum = 100;
-            this.trackBar1.Minimum = -100;
+            this.trackBar1.Maximum = 180;
+            this.trackBar1.Minimum = -180;
             this.trackBar1.Name = "trackBar1";
             this.trackBar1.Size = new System.Drawing.Size(200, 45);
             this.trackBar1.TabIndex = 7;
@@ -131,18 +138,50 @@
             this.pictureBox1.TabIndex = 5;
             this.pictureBox1.TabStop = false;
             // 
+            // trackBar3
+            // 
+            this.trackBar3.LargeChange = 10;
+            this.trackBar3.Location = new System.Drawing.Point(75, 302);
+            this.trackBar3.Maximum = 100;
+            this.trackBar3.Minimum = -100;
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(200, 45);
+            this.trackBar3.TabIndex = 6;
+            this.trackBar3.Scroll += new System.EventHandler(this.TrackBar3_Scroll);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(17, 313);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Lightness";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(292, 313);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(13, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "0";
+            // 
             // HueSaturation
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(325, 345);
+            this.ClientSize = new System.Drawing.Size(325, 388);
             this.ControlBox = false;
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar3);
             this.Controls.Add(this.trackBar2);
             this.Controls.Add(this.trackBar1);
             this.Controls.Add(this.pictureBox1);
@@ -157,6 +196,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -173,5 +213,8 @@
         private System.Windows.Forms.TrackBar trackBar2;
         private System.Windows.Forms.TrackBar trackBar1;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
--- DoAnLTTQ/Forms/HueSaturation.cs
+++ DoAnLTTQ/Forms/HueSaturation.cs
@@ -13,6 +13,7 @@ namespace DoAnLTTQ.Forms
     public partial class HueSaturation : Form
     {
         private Bitmap image;
+        private Bitmap adjusted;
         public HueSaturation()
         {
             InitializeComponent();
@@ -27,19 +28,64 @@ namespace DoAnLTTQ.Forms
             }
             get
             {
-                return image;
+                return new Bitmap(pictureBox1.Image);
             }
+        }
+
+        private void Adjust()
+        {
+            if (adjusted != null)
+            {
+                adjusted.Dispose();
+                adjusted = null;
+            }
+            adjusted = new Bitmap(image);
+            pictureBox1.Image = adjusted;
         }
 
         private void TrackBar1_Scroll(object sender, EventArgs e)
         {
             label3.Text = trackBar1.Value.ToString();
+            Adjust();
+        }
+
+        private void RGBtoHSL(float H, float S, float L, Color c)
+        {
+            float r, g, b;
+            r = c.R / 255;
+            g = c.G / 255;
+            b = c.B / 255;
+            float max = Math.Max(r, Math.Max(g, b));
+            float min = Math.Min(r, Math.Min(g, b));
+            if (max == min) H = 0;
+            else if (max == r) H = 60 * ((g - b) / (max - min));
+            else if (max == g) H = 60 * (2 + (b - r) / (max - min));
+            else if (max == b) H = 60 * (4 + (r - g) / (max - min));
+            if (H < 0) H += 360f;
+            L = (max + min) / 2;
+            if (max == 0 || min == 1)
+                S = 0;
+            else S = (max - min) / (1 - Math.Abs(max + min - 1));
+        }
+
+        private void HSLtoRBG(float H, float S, float L , Color c)
+        {
+            float r, g, b;
+            float C = (1 - Math.Abs(2 * L - 1)) * S;
+            float h = H / 60;
+            float X = C * (1 - Math.Abs(h % 2 - 1));
         }
 
         private void TrackBar2_Scroll(object sender, EventArgs e)
         {
             label4.Text = trackBar2.Value.ToString();
+            Adjust();
         }
 
+        private void TrackBar3_Scroll(object sender, EventArgs e)
+        {
+            label6.Text = trackBar3.Value.ToString();
+            Adjust();
+        }
     }
 }
